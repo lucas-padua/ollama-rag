84e10d0c86655c02a3d8e5e6ca6f83ff8e9ed606
lucas-padua <lucaspadua599@gmail.com>
2025-01-20 21:41:38 -0300

    feat: added simple response evaluation to pipeline

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -1,13 +1,17 @@
-from llama_index.core import SimpleDirectoryReader, PromptTemplate
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import (
+    SimpleDirectoryReader,
+    PromptTemplate,
+)
 
-# from llama_index.core.postprocessor import (
-#     MetadataReplacementPostProcessor,
-#     SentenceTransformerRerank,
-# )
+from llama_index.core.response_synthesizers import TreeSummarize
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.response_synthesizers import ResponseMode
+from llama_index.core.postprocessor import (
+    MetadataReplacementPostProcessor,
+    SentenceTransformerRerank,
+)
 from llama_index.core.settings import Settings
 from llama_index.llms.ollama import Ollama
-from llama_index.core.query_pipeline import QueryPipeline
 from llama_index.core.evaluation import FaithfulnessEvaluator
 from . import chroma_database
 import sys
@@ -21,25 +25,34 @@ class RAGPipeline:
 
         documents = SimpleDirectoryReader(documents_path).load_data()
         self.index = chroma_database.build_index(documents)
+        postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
+        rerank = SentenceTransformerRerank(top_n=2, model="BAAI/bge-reranker-base")
 
-        prompt_string = """You are a doctor, a specialist in endometriosis, that gives direct and concise answers,
-        never gives medical advice and only answers something when the response is found in the context.
-        Given the context and not prior knowledge, answer the query: {query_str}"""
-        prompt_template = PromptTemplate(prompt_string)
+        qa_prompt_template = """
+        You are a Gynecologist, and your expertise is in endometriosis. \n
+        Respond to the query with the provided documents. \n
+        Query: {query_str}. \n
+        Be concise and provide reference if possible.
+        """
+        qa_prompt = PromptTemplate(qa_prompt_template)
 
-        self.pipeline = QueryPipeline(chain=[prompt_template, Settings.llm])
+        response_synthesizer = TreeSummarize(summary_template=qa_prompt)
         self.evaluator = FaithfulnessEvaluator(llm=Settings.llm)
+        self.query_engine = self.index.as_query_engine(
+            similarity_top_k=6,
+            vector_store_query_mode="hybrid",
+            alpha=0.5,
+            node_postprocessor=[postproc, rerank],
+            response_synthetizer=response_synthesizer,
+        )
 
     def query(self, user_query):
-        response = self.pipeline.run(user_query)
-        print(response)
-        # evaluation = self.evaluator.evaluate_response(
-        #     response=response, query=user_query, contexts=response.source_nodes
-        # )
-        # for token in str(response).split(" "):
-        #     print(token, end=" ", flush=True)
-        #     sys.stdout.flush()
-        #     time.sleep(0.4)
-        # print()
-        # print("\n", str(evaluation.passing))
-        # print("\n", str(evaluation.feedback))
+        response = self.query_engine.query(user_query)
+        evaluation = self.evaluator.evaluate_response(response=response)
+        for token in str(response).split(" "):
+            print(token, end=" ", flush=True)
+            sys.stdout.flush()
+            time.sleep(0.4)
+        print()
+        print("\n", str(evaluation.passing))
+        print("\n", str(evaluation.feedback))
